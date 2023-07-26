7f4e93ddc0bd41a4b14b654edb8b9af1771d583b
Pawan Kumar <pawan.3940@gmail.com>
2023-07-26 15:36:13 -0700

    complete lab

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id 	integer PRIMARY KEY,
+    name text,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive integer
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (1,'Mr. Chocolate', 5, 'M', 'Black','Rhythmicity',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (2,'Rowdy', 7, 'F', 'White','Rhythmicity',0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (3,'Tabitha', 3, 'F', 'Cream','Rhythmicity',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (4,'Sergeant Brown', 5, 'M', 'Brown','Rhythmicity',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (5,'Melissa', 4, 'M', 'Blonde','Rhythmicity',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (6,'Grinch',7, 'F', 'Black','Rhythmicity',1);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (7,'Wendy', 9, 'M', 'White','Rhythmicity',0);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (8,NULL, NULL, NULL, NULL,NULL,NULL);
+
